8e0e5709c7eaccb0b0ee7e99c3059acd0b9afcf8
Данил <danilimys@gmail.com>
2025-03-12 19:49:32 +0900

    save work

--- components/app-header.vue
+++ components/app-header.vue
@@ -2,44 +2,56 @@
   <header class="header">
     <div
       class="container flex flex-row flex-justify-content-space-between flex-align-items-center"
+    
       style="height: 52px"
     >
       <img src="~/assets/Logo.svg" class="logo" />
-      <div class="flex flex-row flex-align-items-center">
-        <nav class="nav flex flex-row flex-align-items-center mr-68">
+      <div class="flex flex-row flex-align-items-center"
+      >
+        <nav class="nav flex flex-row flex-align-items-center"
+        v-show="!isMobile"
+        >
           <a
+            v-for="(nav, index) in navItems"
+            :key="index"
             class="text-body-large text-medium cursor-pointer underline-animation nav_button__margin"
-            >Home</a
           >
-          <a
-            class="text-body-large text-medium cursor-pointer underline-animation nav_button__margin"
-            >About</a
-          >
-          <a
-            class="text-body-large text-medium cursor-pointer underline-animation nav_button__margin"
-            >Blog</a
-          >
-          <a
-            class="text-body-large text-medium cursor-pointer underline-animation nav_button__margin"
-            >Pricing</a
-          >
+            {{ nav.name }}
+          </a>
         </nav>
-        <div>
-          <a
-            class="contact-us text-body-large text-semibold underline-animation"
-            >Contact Us -></a
-          >
-        </div>
       </div>
     </div>
   </header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref,onMounted } from "vue";
 
-<style>
+interface NavItem {
+  name: string;
+  path: string;
+}
+
+const navItems = ref<NavItem[]>([
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Blog", path: "/blog" },
+  { name: "Pricing", path: "/pricing" },
+  { name: "Contact Us ->", path: "/contact" },
+]);
+const isMobile = ref(false);
+
+const checkMobile = () => {
+  isMobile.value = window.innerWidth <= 425;
+}
+onMounted(() => {
+  checkMobile();
+  window.addEventListener('resize',checkMobile);
+})
+</script>
+
+<style scoped>
 .header {
-  /* background-color: lightblue; */
   width: 100%;
   height: 114px;
 
@@ -49,10 +61,15 @@
   align-items: center;
   justify-content: center;
 }
-.nav_button__margin{
+
+.nav_button__margin {
   margin-right: 32px;
 }
-.nav_button__margin:last-child{
-  margin-right:68px;
+.nav_button__margin:nth-last-child(2) {
+  margin-right: 68px;
+}
+
+@media (max-width: 425px) {
+
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,20 @@ export default defineNuxtConfig({
   //global styles
   css:[
     '~/assets/index.css'
-  ]
+  ],
+  app:{
+    head:{
+      charset: 'UTF-8', 
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no', 
+      title: 'Coca', 
+      meta: [
+        { name: 'description', content: 'Проверка возможностей Nuxt' },
+        { name: 'robots', content: 'index, follow' },
+      ],
+      // link: [
+      //   // Пример добавления фавиконки
+      //   { rel: 'icon', href: '/favicon.ico' }
+      // ],
+    }
+  }
 })
